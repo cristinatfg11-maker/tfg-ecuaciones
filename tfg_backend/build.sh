6859292a45203617498af804ac22a635f6d44b39
cristinatfg11-maker <cristinatfg11@gmail.com>
2025-12-29 18:38:03 -0500

    Forzar migracion de base de datos en nube

--- tfg_backend/build.sh
+++ tfg_backend/build.sh
@@ -5,10 +5,14 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# --- ESTA LINEA ES LA CLAVE: Actualiza las columnas de la BD ---
+python manage.py makemigrations api
+python manage.py migrate api
 python manage.py migrate
 
 # 1. Crear superusuario 'admin' si no existe
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')"
 
-# 2. EJECUTAR ROBOT DE IMPORTACIÓN (NUEVO)
+# 2. EJECUTAR ROBOT DE IMPORTACIÓN
 python manage.py cargar_ejercicios
