4d89ad84e585886d110f95a42f7afd80bbe7794c
cristinatfg11-maker <cristinatfg11@gmail.com>
2025-12-29 14:00:27 -0500

    Automatizar creacion de superusuario

--- tfg_backend/build.sh
+++ tfg_backend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- LA LINEA MAGICA: Crea el superusuario 'admin' con clave 'admin1234' si no existe ---
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')"
