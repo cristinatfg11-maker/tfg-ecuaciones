0f39c76b2ee74dac768fc1c3881b1da63a646cd3
cristinatfg11-maker <cristinatfg11@gmail.com>
2025-12-29 15:20:37 -0500

    Agregar robot importador automatico

--- tfg_backend/build.sh
+++ tfg_backend/build.sh
@@ -7,5 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# --- LA LINEA MAGICA: Crea el superusuario 'admin' con clave 'admin1234' si no existe ---
+# 1. Crear superusuario 'admin' si no existe
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')"
+
+# 2. EJECUTAR ROBOT DE IMPORTACIÓN (NUEVO)
+python manage.py cargar_ejercicios
