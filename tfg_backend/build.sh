d2590ef290be007b9bf9fcfc05326d2caaac40b5
cristinatfg11-maker <cristinatfg11@gmail.com>
2025-12-29 19:59:11 -0500

    Eliminar robot y reiniciar estructura BD

--- tfg_backend/build.sh
+++ tfg_backend/build.sh
@@ -5,14 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
-# --- ESTA LINEA ES LA CLAVE: Actualiza las columnas de la BD ---
-python manage.py makemigrations api
-python manage.py migrate api
 python manage.py migrate
 
-# 1. Crear superusuario 'admin' si no existe
+# Crear superusuario 'admin' si no existe (esto sí es útil mantenerlo)
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')"
-
-# 2. EJECUTAR ROBOT DE IMPORTACIÓN
-python manage.py cargar_ejercicios
